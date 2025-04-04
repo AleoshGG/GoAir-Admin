66377f596e41578145793b06cb160029df53431a
Alexis Guzman <spartanthelexvro@gmail.com>
2025-04-03 18:19:20 -0600

    Modificando cors

--- API/index.go
+++ API/index.go
@@ -14,7 +14,14 @@ func main() {
 	iAdmin.GoDependences()
 	
 	r := gin.Default()
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // o "*" para pruebas
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	
 
 	Aroutes.RegisterRouter(r)
 	
